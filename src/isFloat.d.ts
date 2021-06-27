a7283fc30287030119d570657395cdd64547926c
Brian Takita <brian.takita@gmail.com>
2021-06-26 23:07:17 -0400

    ## 7.0.9
    ### Patch Changes
    
    - dist directory

--- src/isFloat.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function isFloat(number: any): boolean;
--- src/no_comma_str_.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function no_comma_str_(string: string): string;
-export { no_comma_str_ as _no_comma_str, no_comma_str_ as unformat__commas, };
